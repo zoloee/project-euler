c54adbc8218ba7dd21ea8ece0750d42171631993
zoloee <zoloee@gmail.com>
2017-03-08 10:50:24 +0100

    prOne 1st solution

---
+++ PrOne.java
@@ -0,0 +1,20 @@
+/**
+ * Created by zoloe on 2017. 03. 08..
+ * https://projecteuler.net/problem=1
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+public class PrOne {
+    int sum = 0;
+    int top = 999;
+
+    public void createSum() {
+        for (int i = top; i > 0; i--) {
+            if ((i % 3 == 0 ) || (i % 5 == 0)){
+                sum = sum + i;
+            }
+        }
+        System.out.println(sum);
+    }
+}
